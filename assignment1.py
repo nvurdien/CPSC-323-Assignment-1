8e8a7b7dffdc9d2a84f7d9de564c9cc43cab7cd5
Navie Vurdien <nvurdien@gmail.com>
2018-03-01 16:06:04 -0800

    state table

--- assignment1.py
+++ assignment1.py
@@ -1,18 +1,43 @@
 from enum import Enum
+import string
+
+keyword = ['while']
+separator = ['(', ')', '[', ']', '{', '}']
 
 
 class State(Enum):
-    REJECT = 0,
-    INTEGER = 1,
-    REAL = 2,
-    OPERATOR = 3,
-    STRING = 4,
-    UNKNOWN = 5,
-    SPACE = 6
+    REJECT = 0, 'REJECT'
+    INTEGER = 1, 'INTEGER'
+    REAL = 2, 'REAL'
+    OPERATOR = 3, 'OPERATOR'
+    SEPARATOR = 4, 'SEPARATOR'
+    IDENTIFIER = 5, 'IDENTIFIER'
+    UNKNOWN = 6, 'UNKNOWN'
+    SPACE = 7, 'SPACE'
+
+    def __new__(cls, value, name):
+        member = object.__new__(cls)
+        member._value_ = value
+        member.fullname = name
+        return member
+
+    def __int__(self):
+        return self.value
 
 
 # still working on the states    
-stateTable = [[]]
+stateTable = [
+    [0, State.INTEGER, State.REAL, State.OPERATOR, State.SEPARATOR, State.IDENTIFIER, State.UNKNOWN, State.SPACE],
+    [State.INTEGER, State.INTEGER, State.REAL, State.REJECT, State.REJECT, State.REJECT, State.REJECT, State.REJECT],
+    [State.REAL, State.REAL, State.UNKNOWN, State.REJECT, State.REJECT, State.REJECT, State.REJECT, State.REJECT],
+    [State.OPERATOR, State.REJECT, State.REJECT, State.REJECT, State.REJECT, State.REJECT, State.REJECT, State.REJECT],
+    [State.SEPARATOR, State.REJECT, State.REJECT, State.REJECT, State.REJECT, State.REJECT, State.REJECT, State.REJECT],
+    [State.IDENTIFIER, State.UNKNOWN, State.REJECT, State.REJECT, State.REJECT, State.IDENTIFIER, State.REJECT,
+     State.REJECT],
+    [State.UNKNOWN, State.UNKNOWN, State.UNKNOWN, State.UNKNOWN, State.UNKNOWN, State.UNKNOWN, State.UNKNOWN,
+     State.REJECT],
+    [State.SPACE, State.REJECT, State.REJECT, State.REJECT, State.REJECT, State.REJECT, State.REJECT, State.REJECT],
+]
 
 
 def checkToken(token):
@@ -21,34 +46,37 @@ def checkToken(token):
     elif token.isspace():
         return State.SPACE
     elif token.alpha():
-        return State.STRING
+        return State.IDENTIFIER
     elif token == '.':
         return State.REAL
-    elif token.ispunct():
-        return State.OPERATOR
+    for i in string.punctuation:
+        if token == i:
+            if token in separator:
+                return State.SEPARATOR
+            return State.OPERATOR
     else:
         return State.UNKNOWN
 
 
 def Lexer(expression):
+    global col
     tokens = []
     col = State.REJECT
     currentState = State.REJECT
     prevState = State.REJECT
     currentToken = ""
     for token in expression:
-        value_type = checkToken(token)
+        col = checkToken(token)
+        currentState = stateTable[currentState][col._value_]
         if currentState == State.REJECT:
             if prevState != State.SPACE:
-                temp = {'token': currentToken, 'lexemme': prevState}
-                tokens.append(temp)
+                tokens.append({'token': currentToken, 'lexemme': prevState})
             currentToken = ""
         else:
             currentToken += token
         prevState = currentState
     if currentState != State.SPACE and currentToken != "":
-        temp = {'token': currentToken, 'lexemme': prevState}
-        tokens.append(temp)
+        tokens.append({'token': currentToken, 'lexemme': prevState})
     return tokens
 
 
